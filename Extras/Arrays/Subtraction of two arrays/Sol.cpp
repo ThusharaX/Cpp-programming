295db5ee28e4248537fc395cf71dc444fd6af664
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-04-04 00:18:08 +0530

    Create Sol.cpp

---
+++ Extras/Arrays/Subtraction of two arrays/Sol.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+int A[100] , i , n , B[100] , C[100];
+cout<<"Enter size of A and B";
+cin>>n;
+cout<<"Enter elements of A";
+for(i=0;i<n;i++)
+{
+    cin>>A[i];
+}
+
+cout<<"Enter elements of B";
+for(i=0;i<n;i++)
+{
+    cin>>B[i];
+}
+
+for(i=0;i<n;i++)
+{
+   C[i]=A[i]-B[i];
+   cout<<C[i]<<" ";
+}
+
+
+return 0;
+}
