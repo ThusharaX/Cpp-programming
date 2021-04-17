75bec5053fbea0e4652d277df7947e16f4c328af
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-04-17 10:44:52 +0530

    Create Addition.cpp

---
+++ Addition.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a , b;
+double c , d;
+  
+cout<<"Enter two integers to be added";
+cin>>a>>b;
+cout<<"Enter two decimal numbers to be added";
+cin>>c>>d;
+cout<<"Sum of the two integers :<<" "<<a+b;
+cout<<"Sum of the two decimal numbers :<<" "<<c+d;
+ 
+return 0;
+}
+
+
+  
