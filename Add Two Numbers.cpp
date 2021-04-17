fc2bb6ade14d16b347d53cd37deb2dfd38ad8f5b
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-04-17 11:29:45 +0530

    Create Add Two Numbers.cpp

---
+++ Add Two Numbers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int a , b, n ;
+    cin>>n;
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>a>>b;
+        int sum=a+b;
+        cout<<sum<<endl;
+    }
+    return 0;
+}
