17f0f80338e02d68062024d98b17f1f85258352f
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-04-17 10:49:27 +0530

    Create Product.cpp

---
+++ Product.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a , b;
+double c , d;
+  
+cout<<"Enter two integers to be multiplied";
+cin>>a>>b;
+cout<<"Enter two decimal numbers to be multiplied";
+cin>>c>>d;
+cout<<"Product of the two integers :<<" "<<a*b;
+cout<<"Product of the two decimal numbers :<<" "<<c*d;
+ 
+return 0;
+}
