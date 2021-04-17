df72a54c8c36af199fa12ae4773a5a9947ce1c7a
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-04-17 10:50:58 +0530

    Create Div.cpp

---
+++ Div.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a , b;
+double c , d;
+  
+cout<<"Enter two integers to be divided";
+cin>>a>>b;
+cout<<"Enter two decimal numbers to be divided";
+cin>>c>>d;
+cout<<"Division of the two integers :<<" "<<a/b;
+cout<<"Division of the two decimal numbers :<<" "<<c/d;
+ 
+return 0;
+}
