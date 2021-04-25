9128505347f69b06b7d6b9725ac56d5a763878c2
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-04-25 13:05:21 +0530

    Create INOUT.cpp

---
+++ INOUT.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a ;
+char ch;
+String S;
+  
+cout<<"Enter an integer , character and a String";
+cin>>a>>ch>>S;
+  
+cout<<a<<ch<<S;
+  
+return 0;
+}
