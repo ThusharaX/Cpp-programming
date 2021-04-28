7f8ff6f583d742bc0e2f71e5ced1001c84c00b7d
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-04-28 17:12:16 +0530

    Plane added

---
+++ Past Papers/2020 Assignment 01/THUSHARA/Plane/Plane - Thushara.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+
+using namespace std;
+
+class Plane {
+    private:
+        int planeID;
+        string piolet;
+        string destination;
+    public:
+        void setPlaneDetails(int pID, string p, string d);
+        void displayPlaneDetails();
+        void setPiolet();
+        // void getDestination();
+};
+
+int main() {
+
+    Plane p[4];
+
+    p[0].setPlaneDetails(1, "John", "USA");
+    p[1].setPlaneDetails(2, "George", "UK");
+    p[2].setPlaneDetails(3, "Henry", "USA");
+    p[3].setPlaneDetails(4, "Ronald", "UAE");
+
+    for(int i = 0; i < 4; i++) {
+        p[i].setPiolet();
+    }
+
+    for(int i = 0; i < 4; i++) {
+        p[i].displayPlaneDetails();
+    }
+
+    return 0;
+}
+
+void Plane::setPlaneDetails(int pID, string p, string d) {
+    planeID = pID;
+    piolet = p;
+    destination = d;
+}
+
+void Plane::displayPlaneDetails() {
+    cout << endl <<"PlaneID = " << planeID <<endl;
+    cout << "Piolet = " << piolet <<endl;
+    cout << "Destination = " << destination <<endl;
+}
+
+void Plane::setPiolet() {
+    cout << "Input new piolet of plane " << planeID << ": ";
+    cin >> piolet;
+}
+
+// void Plane::getDestination() {
+
+// }
