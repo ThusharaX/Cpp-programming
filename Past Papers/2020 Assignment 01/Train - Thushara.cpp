84638476cf1eb4b7c86730283a271c1f1b507092
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-04-28 15:17:52 +0530

    Train Q added

---
+++ Past Papers/2020 Assignment 01/Train - Thushara.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+
+using namespace std;
+
+class Train {
+    private:
+        int trainID;
+        int capacity;
+        string startTime;
+        string destination;
+    public:
+        void setTrainDetails(int tID, int c, string sT, string d);
+        void displayTrainDetails();
+        void setStartTime();
+};
+
+int main() {
+
+    Train t[3];
+
+    t[0].setTrainDetails(1, 200, "6:00AM", "Kandy");
+    t[1].setTrainDetails(2, 150, "7:30AM", "Galle");
+    t[2].setTrainDetails(3, 300, "4:00AM", "Jaffna");
+
+    for(int i = 0; i < 3; i++) {
+        t[i].setStartTime();
+    }
+
+    for(int i = 0; i < 3; i++) {
+        t[i].displayTrainDetails();
+    }
+
+    return 0;
+}
+
+void Train::setTrainDetails(int tID, int c, string sT, string d) {
+    trainID = tID;
+    capacity = c;
+    startTime = sT;
+    destination = d;
+}
+
+void Train::displayTrainDetails() {
+    cout << endl <<"TrainID = " << trainID <<endl;
+    cout << "Capacity = " << capacity <<endl;
+    cout << "StartTime = " << startTime <<endl;
+    cout << "Destination = " << destination <<endl;
+}
+
+void Train::setStartTime() {
+    cout << "Input new start time of train " << trainID << ": ";
+    cin >> startTime;
+}
