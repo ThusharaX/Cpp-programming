f548d3e314948f98ecee8a41ed2b326ec02e0bb5
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-04-28 15:30:40 +0530

    Update Dog - Thushara.cpp

--- Past Papers/2020 Assignment 01/Dog - Thushara.cpp
+++ Past Papers/2020 Assignment 01/Dog - Thushara.cpp
@@ -15,19 +15,19 @@ class Dog {
 
 int main() {
 
-    Dog d1, d2, d3;
+    Dog d[3];
 
-    d1.setDogDetails(1, "Zimba", "Lucy");
-    d2.setDogDetails(2, "Timmy", "Kate");
-    d3.setDogDetails(3, "Toby", "Joel");
+    d[0].setDogDetails(1, "Zimba", "Lucy");
+    d[1].setDogDetails(2, "Timmy", "Kate");
+    d[2].setDogDetails(3, "Toby", "Joel");
 
-    d1.setOwner();
-    d2.setOwner();
-    d3.setOwner();
-    
-    d1.displayDogDetails();
-    d2.displayDogDetails();
-    d3.displayDogDetails();
+    for(int i = 0; i < 3; i++) {
+        d[i].setOwner();
+    }
+
+    for(int i = 0; i < 3; i++) {
+        d[i].displayDogDetails();
+    }
 
     return 0;
 }
